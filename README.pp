890dc943d1a9eeb68709de9b215b089ea98e6297
Pranav Pratyush <99145117+Champ2979@users.noreply.github.com>
2023-01-09 00:57:45 +0530

    Update README.pp

--- README.pp
+++ README.pp
@@ -1,1 +1,32 @@
+    <h1 align="center"> Hi 👋, I'm Pranav Pratyush </h1>
+<h3 align="center">A passionate aspiring Web Developer developer from India</h3>
+
+<img align="right" alt="coding" width="400" src="https://cdn.dribbble.com/users/926537/screenshots/4502924/python-2.gif">
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=champ2979&label=Profile%20views&color=0e75b6&style=flat" alt="champ2979" /> </p>
+
+- 🌱 I’m currently learning **The Complete Web Developer Course 2.0**
+
+- 👨‍💻 All of my projects are available at [https://linktr.ee/pranav_pratyush](https://linktr.ee/pranav_pratyush)
+
+- 📫 How to reach me **pranavpratyush2007@gmail.com**
+
+- 📄 Know about my experiences [https://drive.google.com/file/d/1xIYx1qDWc2DfKZhKd8f8UYaO9KsRr09-/view?usp=share_link](https://drive.google.com/file/d/1xIYx1qDWc2DfKZhKd8f8UYaO9KsRr09-/view?usp=share_link)
+
+- ⚡ Fun fact **Sense of humor is quiet good in me**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://twitter.com/pranav_pratyush" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="pranav_pratyush" height="30" width="40" /></a>
+<a href="https://linkedin.com/in/pranav-pratyush" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="pranav-pratyush" height="30" width="40" /></a>
+<a href="https://instagram.com/pranav_pratyush_" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="pranav_pratyush_" height="30" width="40" /></a>
+</p>
+    
+</body>
+</html>
+
+
+
+
+
 
